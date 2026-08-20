fb889e26a0172258c561e0cbc7a5d5d77b7111b9
KDDave97 <kuruczdavid997@gmail.com>
2026-08-21 00:15:34 +0200

    Day 17 WIP

--- Day18/Day18.py
+++ Day18/Day18.py
@@ -4,33 +4,29 @@ import random
 tim = Turtle()
 screen = Screen()
 screen.colormode(255)
-tim.shape("turtle")
 tim.color("coral")
 
-# for i in range(15):
-#     tim.forward(10)
-#     tim.teleport(tim.xcor() + 10, tim.ycor())
 
+tim.pensize(5)
+tim.speed(0)
 
-tim.teleport(-50, tim.ycor() + 350)
-sides = 3
-while sides < 20:
-    angle = 360 / sides
-    for i in range(sides):
-        tim.forward(100)
-        tim.right(angle)
-    sides += 1
+directions = [0, 90, 180, 270]
+
+def rand_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    tim.pencolor(r, g, b)
+    tim.color(r, g, b)
+
+steps = 100
+
+for i in range(steps):
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+    rand_color()
 
 
 
-# turn = 4
-# angle = 360 / turn
-# for i in range(turn):
-#     tim.forward(100)
-#     tim.right(angle)
+
 screen.exitonclick()
 
