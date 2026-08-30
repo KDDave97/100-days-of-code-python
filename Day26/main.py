06a9b9317f55fa3ab94618e5f4619825fa4d0f27
KDDave97 <kuruczdavid997@gmail.com>
2026-08-30 17:11:29 +0200

    Day26 Done

---
+++ Day26/main.py
@@ -0,0 +1,11 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data_dict = {row["letter"]:row["code"] for (index, row) in data.iterrows()}
+
+word = input("Enter a word: ")
+
+word_in_nato = [data_dict[letter.upper()] for letter in word]
+print(word_in_nato)
+
+
