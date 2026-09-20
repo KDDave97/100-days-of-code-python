88f7f84ae7f4c6cf7c0319e00c9bb346a23b907b
KDDave97 <kuruczdavid997@gmail.com>
2026-09-20 21:20:30 +0200

    Day45 Done

--- Day45/100movies.py
+++ Day45/100movies.py
@@ -5,9 +5,7 @@ response = requests.get("https://web.archive.org/web/20200518073855/https://www.
 data = response.text
 
 soup = BeautifulSoup(data, "html.parser")
-
 movies = soup.find_all(name="h3", class_="title")
-
 movies_text = ""
 
 for movie in movies[::-1]:
