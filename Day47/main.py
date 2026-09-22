e868e971a2715df19263c39c79f9b47cbef8fec2
KDDave97 <kuruczdavid997@gmail.com>
2026-09-22 20:55:15 +0200

    Day46-47 Done

---
+++ Day47/main.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+import os
+
+my_email = os.environ.get("SMTP_EMAIL")
+password = os.environ.get("SMTP_PASSWORD")
+
+threshold_price = 100
+
+product_link = "https://appbrewery.github.io/instant_pot/"
+response = requests.get(product_link)
+response.raise_for_status()
+data = response.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+price_whole = float(soup.find(name="span", class_="a-price-whole").getText())
+
+message =(f"The product that you wanted to buy is now under the set threshold!\n"
+          f"You set the threshold to ${threshold_price}, the current price is ${price_whole}!\n"
+          f"Buy it now at {product_link} ")
+
+if price_whole < threshold_price:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject:Price below threshold\n\n"
+                                                                                         f"{message}")
