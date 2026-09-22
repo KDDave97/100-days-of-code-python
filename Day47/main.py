8a9758c1537129c13488a95156b2817678ec3eb3
KDDave97 <kuruczdavid997@gmail.com>
2026-09-22 21:13:11 +0200

    Day46-47 Done

--- Day47/main.py
+++ Day47/main.py
@@ -16,14 +16,16 @@ data = response.text
 soup = BeautifulSoup(data, "html.parser")
 
 price_whole = float(soup.find(name="span", class_="a-price-whole").getText())
+product_name = " ".join(soup.find(name="span", id="productTitle").getText().split())
 
-message =(f"The product that you wanted to buy is now under the set threshold!\n"
-          f"You set the threshold to ${threshold_price}, the current price is ${price_whole}!\n"
+message =(f"{product_name} is available!\n"
+          f"The product that you wanted to buy is now under the set threshold!\n"
+          f"You set the threshold to {threshold_price}, the current price is {price_whole}!\n"
           f"Buy it now at {product_link} ")
 
 if price_whole < threshold_price:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject:Price below threshold\n\n"
-                                                                                         f"{message}")
+        connection.sendmail(from_addr=my_email, to_addrs="kuruczdavid997@gmail.com", msg=f"Subject:Price below threshold\n\n"
+                                                                                         f"{message}".encode("utf-8"))
